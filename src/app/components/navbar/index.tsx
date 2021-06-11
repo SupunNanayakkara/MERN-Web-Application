1a0a829cf9f9ded285d8494a6f2cdfdc94982416
SupunNanayakkara <52200782+SupunNanayakkara@users.noreply.github.com>
2021-06-12 00:42:54 +0530

    Add Nav Items

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Logo } from "../logo";
+import { NavItems } from "./navitems";
 
 const NavbarContainer = styled.div`
     min-heigh: 68px;
     ${tw`
+        w-full
+        min-w-full
+        max-w-2xl
         flex
         flex-row
-        w-full
-        max-w-2xl
         items-center
         lg:pl-12
         lg:pr-12
@@ -24,5 +26,6 @@ export function Navbar() {
         <LogoContainer>
             <Logo />
         </LogoContainer>
+        <NavItems />
     </NavbarContainer>
 }
